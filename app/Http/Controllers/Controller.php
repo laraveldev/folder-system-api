559c9ed4ef6cd9a7289d834b64aa29a220a0bbd5
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-23 23:39:22 +0500

    for file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,58 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function success($data = [], string $message = 'Muvoffaqqiyatli', int $status = 200)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+    protected function responsePagination($paginator, $data = [], string $message = 'Muvoffaqqiyatli', int $status = 200)
+    {
+        if ($paginator instanceof \Illuminate\Pagination\LengthAwarePaginator) {
+            $pagination = [
+                'current_page' => $paginator->currentPage(),
+                'total_pages' => $paginator->lastPage(),
+                'total' => $paginator->total(),
+                'per_page' => $paginator->perPage(),
+                'links' => [
+                    'first' => $paginator->url(1),
+                    'last' => $paginator->url($paginator->lastPage()),
+                    'prev' => $paginator->previousPageUrl(),
+                    'next' => $paginator->nextPageUrl(),
+                ],
+            ];
+        } else {
+            $pagination = null;
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+            'pagination' => $pagination,
+        ], $status);
+    }
+    protected function error(string $message = 'An error occurred', int $status = 400)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => $message,
+        ], $status);
+    }
+    public function uploadPhoto($file, $path = 'uploads')
+    {
+        $photoName = time() . '.' . $file->getClientOriginalExtension();
+        return $file->storeAs($path, $photoName, 'public');
+    }
+    public function deletePhoto($path)
+    {
+        $fullpath = storage_path('app/public/' . $path);
+        if (file_exists($fullpath)) {
+            unlink($fullpath);
+        }
+        return;
+    }
 }
