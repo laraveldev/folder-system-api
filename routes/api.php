a1a0aa90b8e1ad21719f2d7fc5e242a52ad4ee34
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-23 23:42:09 +0500

    middleware anda routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FolderController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::apiResource('/folders', FolderController::class);
+    Route::get('/search', [FolderController::class, 'search']);
+    Route::delete('/logout', [AuthController::class, 'logout']);
+});
+
